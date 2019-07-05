2780a264a0970aa4495fdf23736aa1faf841bb40
Jean Barry <jfvbarry@gmail.com>
2019-07-05 11:27:47 -0500

    Add favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,8 @@
  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cloudy</title>
+    <title>Kloudy</title>
+    <link rel="shortcut icon" href="{{ asset("assets/img/favicon.ico") }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans">
